52cb45d42095dd0a0cc7062e6a4d4f0eb2eb7bb1
Gerardo <ger.polancomolina@gmail.com>
2026-01-24 21:12:14 +0100

    feat(nvim): Updated colorscheme

--- .config/nvim/lua/roger/plugins/colorscheme.lua
+++ .config/nvim/lua/roger/plugins/colorscheme.lua
@@ -1,8 +1,23 @@
 return {
-    "morhetz/gruvbox",
-    priority = 1000,
-    lazy = false,
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,        -- load during startup
+    priority = 1000,     -- make sure it loads before other UI stuff
     config = function()
-        vim.cmd.colorscheme("gruvbox")
-    end
+      -- REQUIRED: set these *before* colorscheme
+      vim.g.gruvbox_material_background = "soft" -- soft | medium | hard
+      vim.g.gruvbox_material_foreground = "material" -- material | mix | original
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_enable_bold = true
+
+      -- Optional but recommended
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+      vim.g.gruvbox_material_diagnostic_text_highlight = true
+      vim.g.gruvbox_material_diagnostic_line_highlight = false
+      vim.g.gruvbox_material_diagnostic_sign_highlight = true
+
+      -- Load the colorscheme
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 }
